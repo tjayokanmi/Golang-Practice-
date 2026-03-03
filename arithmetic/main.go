012ff1987903e59c03dd05253e7ad3b91ab12cef
Theophilus Jacob <theophilusjacob@gmail.com>
2026-03-03 13:30:07 +0000

    Arithmetic operation

--- arithmetic/main.go
+++ arithmetic/main.go
@@ -9,4 +9,14 @@ func main() {
 	fmt.Println("x * y = ", x*y)
 	fmt.Println("x / y = ", (x / y))
 	fmt.Println("x mod y = ", x%y)
+
+	isbool := true
+	hate := false
+	fmt.Println(isbool && hate) // True and False should give False
+	fmt.Println(isbool || hate) // True or False should give True
+	fmt.Println(!hate)          // Not false is true
+	fmt.Println(!isbool)        // Not true is false
+
 }
+
+// This is a short exercise from Edureka Golang toturial on youtube "https://youtu.be/Q0sKAMal4WQ?t=770"
