597a6812a9c896842ac5a2155f6294bcef6015c3
Theophilus Jacob <theophilusjacob@gmail.com>
2026-03-03 13:18:10 +0000

    Arithmetic operation

---
+++ arithmetic/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	x, y := 5, 6
+	fmt.Println("x + y = ", x+y)
+	fmt.Println("x - y = ", x-y)
+	fmt.Println("x * y = ", x*y)
+	fmt.Println("x / y = ", (x / y))
+	fmt.Println("x mod y = ", x%y)
+}
